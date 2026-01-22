335881e5d36d3593f4901cde85ed8748faa52a8f
csjones <csjones@users.noreply.github.com>
2026-01-22 15:40:05 -0800

    Adding deps

--- Package.swift
+++ Package.swift
@@ -9,13 +9,18 @@ let package = Package(
         .library(name: "Tor", targets: ["Tor"])
     ],
     dependencies: [
-        .package(url: "https://github.com/21-DOT-DEV/swift-plugin-subtree.git", exact: "0.0.7")
+        .package(url: "https://github.com/21-DOT-DEV/swift-plugin-subtree.git", exact: "0.0.7"),
+        .package(url: "https://github.com/21-DOT-DEV/swift-openssl.git", branch: "main"),
+        .package(url: "https://github.com/21-DOT-DEV/swift-event.git", branch: "main"),
     ],
     targets: [
         .target(
             name: "libtor",
             cSettings: [
                 // C settings will be configured after analyzing Tor's build system
+            ],
+            linkerSettings: [
+                .linkedLibrary("z") // zlib => -lz
             ]
         ),
         .target(
